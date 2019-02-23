c43cb53662e6be2b53c94d34d27a1bfa02c5751d
Zach Azar <zach.azar@gmail.com>
2019-02-23 14:54:09 -0800

    Add skills type and helper type for hooks

--- src/components/experience-tree/lib/types.js
+++ src/components/experience-tree/lib/types.js
@@ -1,10 +1,13 @@
 // @flow
 
-export type ExperienceData = {
+export type ExperienceData = {|
 	_id: number,
 	title: string,
 	subtitle: string,
 	tertiaryTitle: string,
 	descriptionTitle?: string,
 	description: string,
-}
+	skills: { [skillName: string]: 'MAX' | 'HIGH' | 'MED' | 'Low' },
+|}
+
+export type useStateFn<T> = ((T => T) | T) => void
