ce07b7e010a378a982c76227f25331d55d3de99f
Zach Azar <zach.azar@gmail.com>
2019-02-23 14:54:40 -0800

    Add animation and active toggling

--- src/components/experience-tree/components/experience-card/index.js
+++ src/components/experience-tree/components/experience-card/index.js
@@ -1,25 +1,55 @@
 // @flow
 import React from 'react'
 import styles from './styles.module.scss'
-import type { ExperienceData } from '../../lib/types'
+import { Transition } from 'react-transition-group'
+import type { ExperienceData, useStateFn } from '../../lib/types'
 
-const ExperienceCard = ({ experienceData }: Props) => (
-	<div className={styles.container}>
+const defaultStyle = {
+	transition: `opacity 100ms ease`,
+	transition: `display 500ms ease`,
+	opacity: 0,
+	display: 'none',
+}
+
+const transitionStyles = {
+	entering: { opacity: 0.4, display: 'inline-block' },
+	entered: { opacity: 1, display: 'inline-block' },
+	exiting: { opacity: 0.4, display: 'inline-block' },
+}
+
+const ExperienceCard = ({ experienceData, isActive, setActive }: Props) => (
+	<div
+		className={styles.container}
+		onClick={() => (isActive ? setActive(0) : setActive(experienceData._id))}
+	>
 		<div className={styles.title}>
 			<div className={styles.mainTitle}>{experienceData.title}</div>
 			<div className={styles.tertiaryTitle}>{experienceData.tertiaryTitle}</div>
 		</div>
-		{experienceData.descriptionTitle && (
-			<div className={styles.desctiptionTitle}>
-				{experienceData.descriptionTitle}
-			</div>
-		)}
-		<div className={styles.description}>{experienceData.description}</div>
+		<Transition in={isActive} timeout={100}>
+			{state => (
+				<div
+					style={{
+						...defaultStyle,
+						...transitionStyles[state],
+					}}
+				>
+					{experienceData.descriptionTitle && (
+						<div className={styles.desctiptionTitle}>
+							{experienceData.descriptionTitle}
+						</div>
+					)}
+					<div className={styles.description}>{experienceData.description}</div>
+				</div>
+			)}
+		</Transition>
 	</div>
 )
 
 type Props = {
 	experienceData: ExperienceData,
+	isActive: boolean,
+	setActive: useStateFn<number>,
 }
 
 export default ExperienceCard
