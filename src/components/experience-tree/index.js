2c09c114be675bb7d69ebfea25f9342fd5dad541
Zach Azar <zach.azar@gmail.com>
2019-02-26 22:15:49 -0800

    Move data to file - add more examples

--- src/components/experience-tree/index.js
+++ src/components/experience-tree/index.js
@@ -5,62 +5,38 @@ import ExperienceCard from './components/experience-card'
 import SkillTree from './components/skill-tree'
 import type { ExperienceData } from './lib/types'
 import { SizeMe } from 'react-sizeme'
-
-const education: Array<ExperienceData> = [
-	{
-		_id: 1,
-		title: 'M.S. in Computer Science',
-		subtitle: 'University of Denver (2013 - 2016) Denver, CO',
-		tertiaryTitle: 'GPA: 3.97',
-		descriptionTitle:
-			'Thesis: PECCit - An Omniscient Debugger for Web Development',
-		description:
-			'PECCit is a multi-application software system which offers a tool for improved backend debugging. Built using PHP, C++, Linux, MySQL, JavaScript, jQuery, AJAX, a REST API, HTML, CSS, Bootstrap, etc.',
-		skills: {
-			PHP: 'HIGH',
-			MySQL: 'HIGH',
-			'Advanced Data Structures and Algorithms': 'MAX',
-		},
-	},
-	{
-		_id: 2,
-		title: 'B.S. in Computer Science',
-		subtitle: 'Vanderbilt University (2007 - 2011) Nashville, TN',
-		tertiaryTitle: 'GPA: 3.70',
-		description:
-			"Minors: Engineering Management, Mathematics\nHonors: Cum Laude, Honors in Computer Science, Dean's List",
-		skills: {
-			'Object Oriented Programming': 'MAX',
-			Java: 'HIGH',
-			'C++': 'HIGH',
-		},
-	},
-]
+import { education, workExperience, projects } from './lib/experience-data'
 
 const ExperienceTree = () => {
 	const [active, setActive] = React.useState(0)
-	const experienceCards = education.map(experienceData => (
+
+	const experienceDataToCard = experienceData => (
 		<ExperienceCard
 			key={experienceData._id}
 			isActive={experienceData._id === active}
 			experienceData={experienceData}
 			setActive={setActive}
 		/>
-	))
+	)
+	const educationCards = education.map(experienceDataToCard)
+	const workExperienceCards = workExperience.map(experienceDataToCard)
+	const projectCards = projects.map(experienceDataToCard)
 	return (
 		<div className={styles.container}>
 			<div className={styles.panel} id="leftPanel">
 				<h2>Education</h2>
-				{experienceCards}
+				{educationCards}
 				<h2>Experience</h2>
+				{workExperienceCards}
 				<h2>Projects</h2>
+				{projectCards}
 			</div>
 			<div className={styles.panel} id="rightPanel">
 				<h2>Technologies and Languages</h2>
 				<SizeMe>
 					{({ size }) => (
 						<SkillTree
-							experiences={education}
+							experiences={[...education, ...workExperience, ...projects]}
 							active={active}
 							width={size.width}
 						/>
---
+++ src/components/experience-tree/lib/experience-data.js
@@ -0,0 +1,64 @@
+// @flow
+import type { ExperienceData } from './types'
+
+export const education: Array<ExperienceData> = [
+	{
+		_id: 1,
+		title: 'M.S. in Computer Science',
+		subtitle: 'University of Denver (2013 - 2016)',
+		tertiaryTitle: 'GPA: 3.97',
+		descriptionTitle:
+			'Thesis: PECCit - An Omniscient Debugger for Web Development',
+		description:
+			'PECCit is a multi-application software system which offers a tool for improved backend debugging. Built using PHP, C++, Linux, MySQL, JavaScript, jQuery, AJAX, a REST API, HTML, CSS, Bootstrap, etc.',
+		skills: {
+			PHP: 'HIGH',
+			MySQL: 'HIGH',
+			'Advanced Data Structures and Algorithms': 'MAX',
+		},
+	},
+	{
+		_id: 2,
+		title: 'B.S. in Computer Science',
+		subtitle: 'Vanderbilt University (2007 - 2011)',
+		tertiaryTitle: 'GPA: 3.70',
+		description:
+			"Minors: Engineering Management, Mathematics\nHonors: Cum Laude, Honors in Computer Science, Dean's List",
+		skills: {
+			'Object Oriented Programming': 'MAX',
+			Java: 'HIGH',
+			'C++': 'HIGH',
+		},
+	},
+]
+
+export const workExperience: Array<ExperienceData> = [
+	{
+		_id: 3,
+		title: 'Co-Founder and Lead Engineer',
+		subtitle: 'Lever Insights, LLC',
+		tertiaryTitle: 'Feb 2016 - July 2016',
+		description:
+			'- Designed a full-stack solution for a data visualization web application-Developed a Single-Page Application (SPA) prototype using Node.js, Express, AngularJS, D3, Bootstrap, Sass',
+		skills: {
+			AngularJS: 'MAX',
+			D3: 'HIGH',
+			Bootstrap: 'MED',
+		},
+	},
+]
+
+export const projects: Array<ExperienceData> = [
+	{
+		_id: 7,
+		title: 'Homepage',
+		subtitle: 'Angular - version 1',
+		tertiaryTitle: 'https://github.com/zachazar/homepage',
+		description:
+			'Built a personal homepage (this website) using Angular, D3, Bootstrap, PHP, Webpack, Sass',
+		skills: {
+			AngularJS: 'MAX',
+			Bootstrap: 'HIGH',
+		},
+	},
+]
