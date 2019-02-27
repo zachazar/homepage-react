9d2d162a57d239e3d3dde60968d2dea675ae9945
Zach Azar <zach.azar@gmail.com>
2019-02-26 21:13:37 -0800

    Remove unneeded references

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 // @flow
 
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 import ExperienceTree from '../components/experience-tree'
 
