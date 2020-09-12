73db9906287cc0b057c58d1ecb1f1f2872e906f2
Zach Azar <zach.azar@gmail.com>
2020-09-12 16:03:08 -0700

    Add Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = {
 	plugins: [
 		'gatsby-plugin-flow',
 		'gatsby-plugin-react-helmet',
+		{
+			resolve: 'gatsby-plugin-google-analytics',
+			options: {
+				trackingId: 'UA-81758415-2',
+			},
+		},
 		'gatsby-plugin-sass',
 		{
 			resolve: 'gatsby-source-filesystem',
