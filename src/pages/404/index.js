8d956de49b78eeff39e41ff7d1e7f442ee43bd0c
Zach Azar <zach.azar@gmail.com>
2021-03-30 06:10:30 -0700

    Customize 404 page

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import cx from 'classnames'
 
 import Layout from '../../components/layout'
@@ -12,9 +13,46 @@ const NotFoundPage = () => (
 		<div className={styles.container}>
 			<div className={cx('title', 'has-text-weight-light')}>Page not found</div>
 			<p>
-				Looks like that page doesn&apos;t exist.{' '}
-				<Link to="/">Click here to go to the homepage.</Link>
+				Looks like that page doesn&apos;t exist.
+				<Link to="/"> Click here to go to the homepage.</Link>
 			</p>
+			<div className={styles.fogContainer}>
+				<p>Maybe it was lost in the fog?</p>
+				<StaticImage
+					src="../../images/fog-404.jpg"
+					alt="A foggy lake with some boats"
+					layout="fullWidth"
+					placeholder="blurred"
+				/>
+			</div>
+			<div className="card">
+				<div className="card-content">
+					<div className={cx('media', styles.media)}>
+						<div className="media-left">
+							<StaticImage
+								src="../../images/favicon-circle-small.png"
+								alt="Photo of Zach"
+								className="image is-48x48"
+								placeholder="blurred"
+							/>
+						</div>
+						<div className="media-content">
+							<p className="title has-text-weight-light is-4">Zach Azar</p>
+							<p className="subtitle is-6">
+								<a target="blank" href="https://twitter.com/zachrazar">
+									@zachrazar
+								</a>
+							</p>
+						</div>
+					</div>
+					<div className="content">
+						<p>
+							Let me know if this seems like an error using{' '}
+							<Link to="/contact">the contact page</Link>. Thank you!
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</Layout>
 )
