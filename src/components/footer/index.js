1c2c7f2800a64b58d54fd13f87ab35ece6532189
Zach Azar <zach.azar@gmail.com>
2021-04-24 11:18:50 -0700

    Use Outbound link tracking in footer links

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,20 +1,21 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
 
 import * as styles from './styles.module.scss'
 
 const Footer = () => (
 	<footer className={styles.footer}>
 		<div className={styles.center}>
-			<a target="blank" href="https://www.linkedin.com/in/zach-azar">
+			<OutboundLink target="blank" href="https://www.linkedin.com/in/zach-azar">
 				<i className="fab fa-linkedin-in" />
-			</a>
-			<a target="blank" href="https://github.com/zachazar/">
+			</OutboundLink>
+			<OutboundLink target="blank" href="https://github.com/zachazar/">
 				<i className="fab fa-github" />
-			</a>
-			<a target="blank" href="https://twitter.com/zachrazar">
+			</OutboundLink>
+			<OutboundLink target="blank" href="https://twitter.com/zachrazar">
 				<i className="fab fa-twitter" />
-			</a>
+			</OutboundLink>
 		</div>
 		<div className={styles.copyright}>
 			© 2021 Zach Azar | <Link to="/privacy-policy">Privacy Policy</Link> |{' '}
